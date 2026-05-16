c4bc8a024efe2b89d36c59dd9e243c6d93781272
asma142-sd <asmasaeed3232@gmail.com>
2026-05-16 20:24:37 +0500

    Updated

---
+++ src/database/DBConnection.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package database;
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    public static Connection getConnection() {
+    try {
+        // Manually load the driver now that JARs are fixed
+        Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+        
+        // Use the absolute path to your file
+        String url = "jdbc:ucanaccess://D:/Documents/NetBeansProjects/E-Commerce/src/Ecommerce.accdb";
+        
+        Connection conn = DriverManager.getConnection(url);
+        System.out.println("Connection Success!");
+        return conn;
+    } catch (ClassNotFoundException | SQLException e) {
+        System.out.println("Error: " + e.getMessage());
+        return null;
+    }
+}
+}
+
